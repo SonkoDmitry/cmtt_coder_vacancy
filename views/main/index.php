c0ea8b9740217c78a6fb985ce100eee2dca052d8
Sonko Dmitry <st.difor@gmail.com>
2014-05-01 23:14:05 +0600

    Add link to source element

--- views/main/index.php
+++ views/main/index.php
@@ -2,16 +2,18 @@
 /**
  * @var yii\web\View $this
  * @var \app\models\Links $model
+ * @var \app\models\Links $media
  */
 $this->title = Yii::$app->name;
 echo '<h1>Самые популярные репортажи:</h1>';
 echo '<ul class="medias">';
-foreach ($model as $media){
+foreach ($model as $media) {
 	echo '<li style="display: inline-block">';
-	echo '<a href="' . $media->link . '" target="_blank" title="' . $media->news_title . '""><h2>' . $media->news_title . '</h2></a>'.(!empty($media->news_pic) ? '<img style="margin-right: 10px;" src="' . $media->news_pic . '" alt="' . $media->news_title . '" align="left">' : '');
+	echo '<a href="' . $media->link . '" target="_blank" title="' . $media->news_title . '""><h2>' . $media->news_title . '</h2></a>' . (!empty($media->news_pic) ? '<img style="margin-right: 10px;" src="' . $media->news_pic . '" alt="' . $media->news_title . '" align="left">' : '');
 	if (!empty($media->news_description)) {
 		echo $media->news_description . '&nbsp;<a href="' . $media->link . '" target="_blank" title="' . $media->news_title . '"">Подробнее</a><br><br>';
 	}
+	echo 'Источник: <a href="' . $media->link . '" target="_blank">' . $media->site->domain . '</a><br>';
 	echo $media->news_total_shares . ' упоминаний: ';
 	echo '<img src="//s1.static.twijournal.com/main/img/icon-tw-12-gray.png">&nbsp;' . $media->news_tw_shares;
 	echo '&nbsp;<img src="//s2.static.twijournal.com/main/img/icon-vk-12-gray.png">&nbsp;' . $media->news_vk_shares;

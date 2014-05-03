63aca27e5bb24fac0e23cf2079422223245a8d30
Sonko Dmitry <st.difor@gmail.com>
2014-05-03 13:07:10 +0600

    Change calculate compare func

--- commands/CompareWordsController.php
+++ commands/CompareWordsController.php
@@ -24,6 +24,8 @@ class CompareWordsController extends \yii\console\Controller
 		}
 		/**
 		 * @var Links $link
+		 * @var Words $firstWordTitle
+		 * @var Words $secondWordTitle
 		 */
 		foreach ($linksModel as $link) {
 			//берем все слова из текущей новости
@@ -31,40 +33,58 @@ class CompareWordsController extends \yii\console\Controller
 					$query->andWhere("link_id=" . $link->id . "");
 					//$query->andWhere("type='title'");
 				}])->all() as $firstWordTitle) {
+				//echo "Compare word: ".$firstWordTitle->word." (".$firstWordTitle->id."), link_id: ".$firstWordTitle->wordsLinks->link_id."\n";
 				/**
 				 * @var Words $firstWordTitle
 				 */
 				//2 сравниваем слова текущей новости со словами из других новостей
-				foreach (Words::find()->joinWith(['wordsLinks' => function ($query) use ($link) {
+				foreach (Words::find()->joinWith(['wordsLinks' => function ($query) use ($link, $firstWordTitle) {
 						$query->andWhere("link_id!=" . $link->id . "");
+						$query->andWhere("word_id=" . $firstWordTitle->id . "");
 						//$query->andWhere("type='title'");
 					}])->all() as $secondWordTitle) {
-					/**
-					 * @var Words $secondWordTitle
-					 */
-					$percent = 0;
-					$result = similar_text($firstWordTitle->word, $secondWordTitle->word, $percent);
-					$record = new WordsCompare();
-					$record->link_first_id = $link->id;
-					$record->word_first_id = $firstWordTitle->id;
-					$record->link_second_id = $secondWordTitle->wordsLinks->link_id;
-					$record->word_second_id = $secondWordTitle->id;
-					$record->type = 'custom';
-					$record->compare = round($percent, 2);
-					$record->added = new Expression('NOW()');
-					$record->save();
+					//echo "Finded in link: ".$secondWordTitle->wordsLinks->link_id."\n";
+					$sqlCheckFirst = "SELECT 1
+FROM " . WordsCompare::tableName() . "
+WHERE
+	`link_first_id`={$firstWordTitle->wordsLinks->link_id} AND
+	`word_first_id`={$firstWordTitle->id} AND
+	`link_second_id`={$secondWordTitle->wordsLinks->link_id} AND
+	`word_second_id`={$secondWordTitle->id}";
 
-					$percent = 0;
-					$result = similar_text($secondWordTitle->word, $firstWordTitle->word, $percent);
-					$record = new WordsCompare();
-					$record->link_first_id = $secondWordTitle->wordsLinks->link_id;
-					$record->word_first_id = $secondWordTitle->id;
-					$record->link_second_id = $link->id;
-					$record->word_second_id = $firstWordTitle->id;
-					$record->type = 'custom';
-					$record->compare = round($percent, 2);
-					$record->added = new Expression('NOW()');
-					$record->save();
+					$checkFirst = \Yii::$app->db->createCommand($sqlCheckFirst)->queryScalar();
+					if ($checkFirst === false) {
+						$record = new WordsCompare();
+						$record->link_first_id = $firstWordTitle->wordsLinks->link_id;
+						$record->word_first_id = $firstWordTitle->id;
+						$record->link_second_id = $secondWordTitle->wordsLinks->link_id;
+						$record->word_second_id = $secondWordTitle->id;
+						$record->type = 'custom';
+						$record->compare = 100;
+						$record->added = new Expression('NOW()');
+						$record->save();
+					}
+
+					$sqlCheckSecond = "SELECT 1
+FROM " . WordsCompare::tableName() . "
+WHERE
+	`link_first_id`={$secondWordTitle->wordsLinks->link_id} AND
+	`word_first_id`={$secondWordTitle->id} AND
+	`link_second_id`={$firstWordTitle->wordsLinks->link_id} AND
+	`word_second_id`={$firstWordTitle->id}";
+
+					$checkFirst = \Yii::$app->db->createCommand($sqlCheckSecond)->queryScalar();
+					if ($checkFirst === false) {
+						$record = new WordsCompare();
+						$record->link_first_id = $secondWordTitle->wordsLinks->link_id;
+						$record->word_first_id = $secondWordTitle->id;
+						$record->link_second_id = $firstWordTitle->wordsLinks->link_id;
+						$record->word_second_id = $firstWordTitle->id;
+						$record->type = 'custom';
+						$record->compare = 100;
+						$record->added = new Expression('NOW()');
+						$record->save();
+					}
 				}
 			}
 			$link->completed = 1;
--- commands/WordsParserController.php
+++ commands/WordsParserController.php
@@ -11,6 +11,54 @@ class WordsParserController extends \yii\console\Controller
 {
 	public function actionIndex()
 	{
+		$okonch = [
+			'ий',
+			'ов',
+			'ей',
+			'ев',
+			'иев',
+			'ам',
+			'ям',
+			'иям',
+			'ом',
+			'ем',
+			'ами',
+			'ями',
+			'ах',
+			'ях',
+			'ый',
+			'ая',
+			'ое',
+			'ые',
+			'ого',
+			'ой',
+			'ых',
+			'ому',
+			'ым',
+			'ыми',
+			'ий',
+			'яя',
+			'ее',
+			'ие',
+			'его',
+			'ей',
+			'их',
+			'ему',
+			'им',
+			'ешь',
+			'ет',
+			'ем',
+			'ете',
+			'ут',
+			'ют',
+			'ишь',
+			'ит',
+			'им',
+			'ите',
+			'ат',
+			'ят',
+		];
+		$regex = '/([' . implode('|', array_unique($okonch)) . ']|[уеаоэяию])*$/i';
 		/**
 		 * @var $model Links
 		 */
@@ -20,6 +68,7 @@ class WordsParserController extends \yii\console\Controller
 			$link_title = preg_replace('/([^a-zа-я0-9\s-])/u', '', $link_title);
 			$titles = explode(' ', mb_strtolower($link_title, 'UTF-8'));
 			foreach ($titles as $title) {
+				$title = preg_replace($regex, '', $title);
 				if (mb_strlen($title, 'UTF-8') < 3) {
 					continue;
 				}
@@ -50,6 +99,7 @@ class WordsParserController extends \yii\console\Controller
 			$link_desc = preg_replace('/([^a-zа-я0-9\s-])/u', '', $link_desc);
 			$descs = explode(' ', mb_strtolower($link_desc, 'UTF-8'));
 			foreach ($descs as $desc) {
+				$desc = preg_replace($regex, '', $desc);
 				if (mb_strlen($desc, 'UTF-8') < 3) {
 					continue;
 				}
